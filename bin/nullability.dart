c85a8eb0dfb278bf1d057517b0d5146ac9e37b28
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-10 17:03:01 +0200

    nullable types

---
+++ bin/nullability.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments) {
+  int? age;
+  double? height;
+  String? message;
+  print(age);
+  print(height);
+  print(message);
+
+  String? profession;
+  print(profession);
+  profession = 'Dart/Flutter Developer';
+  print(profession);
+}
